c8e3f7e18692de5372070dd986aab12439ddac12
Anthony Okpako <melowsworld@yahoo.co.uk>
2020-09-11 13:08:53 +0100

    first commit

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { useValue, onScrollEvent, interpolateColor } from "react-native-redash";
+
+import Slide, { SLIDE_HEIGHT } from "../../components/Slide";
+import Animated, { interpolate } from "react-native-reanimated";
+
+const { width, height } = Dimensions.get("window");
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  slider: {
+    height: SLIDE_HEIGHT,
+    backgroundColor: "cyan",
+    borderBottomRightRadius: 75
+  },
+  footer: {
+    flex: 1
+  }
+});
+
+const slides = [
+  { label: "Relaxed", color: "#BFEAF5" },
+  { label: "Playful", color: "#BEECC4" },
+  { label: "Excentric", color: "#FFE4D9" },
+  { label: "Funky", color: "#FFDDDD" }
+];
+
+const Onboarding = () => {
+  const x = useValue(0);
+  // TODO: useScrollEvent?
+  const onScroll = onScrollEvent({ x });
+  const backgroundColor = interpolateColor(x, {
+    inputRange: slides.map((_, i) => i * width),
+    outputRange: slides.map(slide => slide.color)
+  });
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.slider, { backgroundColor }]}>
+        <Animated.ScrollView
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          scrollEventThrottle={1}
+          {...{ onScroll }}
+        >
+          {slides.map(({ label }, index) => (
+            <Slide key={index} right={!(index % 2)} {...{ label }} />
+          ))}
+        </Animated.ScrollView>
+      </Animated.View>
+      <View style={styles.footer}>
+        <Animated.View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor }}
+        />
+        <View
+          style={{ flex: 1, backgroundColor: "white", borderTopLeftRadius: 75 }}
+        ></View>
+      </View>
+    </View>
+  );
+};
+
+export default Onboarding;
